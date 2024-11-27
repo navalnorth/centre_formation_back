6868f72e27257b5a3b12659a31e272b81dcf150a
unknown <moha_naval@live.com>
2024-11-27 16:18:29 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+let db = null;
+
+const connectToDb = async () => {
+    const timeStamp = new Date();
+    const timeOnly = timeStamp.toLocaleTimeString(); // Format par défaut
+
+    if (db) {
+        console.log(timeOnly, 'Dèja connecté à la base de données');
+        return db;
+    }
+
+    try {
+        db = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+        });
+
+        console.log(timeOnly, 'Connection à la BDD.');
+        return db;
+    } catch (error) {
+        console.error(timeOnly, 'Errur lors de la connexion à la BDD: ', error);
+        db = null;
+        return null;
+    }
+};
+
+module.exports = connectToDb;
