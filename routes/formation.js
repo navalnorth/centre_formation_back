2bc45cd4e84c9fea1d344bcdc1a7e8421a597038
yamine59 <yamineizi22@gmail.com>
2024-12-05 14:11:21 +0100

    test: envprod

--- routes/formation.js
+++ routes/formation.js
@@ -5,6 +5,19 @@ require("dotenv").config();
 const connectToDb = require("../db.js");
 
 
+router.get("/", async (req, res) => {
+    try {
+        const db = await connectToDb();
+        if (!db) { return res.status(500).json({ message: "Erreur de connexion à la base de données" }) }
+
+        const sql = "SELECT * FROM formation";
+        const [results] = await db.query(sql);
+
+        res.status(200).json({ message: "Données récupérées avec succès !", data: results });
+    } catch (err) {
+        res.status(500).json({ message: "Erreur lors de la récupération des données", error: err.message });
+    }
+});
 
 router.get("/:id_formation", async (req, res) => {
     try {
